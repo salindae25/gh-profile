6f81327db34ceea2c5614746bbca3cfa0fdeb3b0
salindae25 <salindae25@gmail.com>
2022-02-20 15:42:48 +0530

    feature:[qa] profile basic structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,22 @@
+import { Card, PercentageBar } from "@/atoms";
+import ExperiencesCard from "@/organisms/ExperiencesCard";
+import IntroductionCard from "@/organisms/IntroductionCard";
+import SkillCard from "@/organisms/SkillCard";
 import { useState } from "react";
 import "./App.css";
-const envTitle = import.meta.env.VITE_APP_TITLE;
 function App() {
-  console.log(import.meta.env);
-
   return (
-    <div className="app">
-      <div className="app_nav">
-        <img
-          src={`${import.meta.env.BASE_URL}logo.svg`}
-          alt="logo"
-          className="app_logo"
-        />
-        <h2 className="app_title">Salinda's Profile</h2>
+    <div className="app font-montserrat bg-gray-1 py-[70px] px-[140px] gap-5 grid overflow-x-hidden">
+      {/* IntroductionCard */}
+      <IntroductionCard />
+      {/* SkillCard */}
+      <div className="grid grid-cols-2 gap-5">
+        <SkillCard />
+        <ExperiencesCard />
       </div>
-      <div>{envTitle}</div>
+      {/* HobbiesCard */}
+      {/* BlogCard */}
+      {/* Projects */}
     </div>
   );
 }
